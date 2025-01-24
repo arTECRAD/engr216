9bf7c900145285c6e033faed9d7abc665746e5d5
Colin Shirley <colin@colinshirley.com>
2025-01-24 16:09:53 -0600

    switched to pstdev and pvariance after clarification

--- HW1/main.py
+++ HW1/main.py
@@ -1,6 +1,6 @@
 import csv 
 import math
-from statistics import median, mean, mode, variance, stdev
+from statistics import median, mean, mode, pvariance, pstdev
 
 def read_column(dataset, column):
     '''
@@ -52,16 +52,16 @@ def main():
 
     # Find the range, variance, and stdev of the fine aggregate component (PART B)
     print(f'PART B: Range of Fine Aggregate Component = {max(fine_ag_data) - min(fine_ag_data)} kg/m^3 mixture')
-    print(f'PART B: Variance of Fine Aggregate Component = {variance(fine_ag_data)} kg/m^3 mixture')
-    print(f'PART B: Standard Deviation of Fine Aggregate Component = {stdev(fine_ag_data)} kg/m^3 mixture\n')
+    print(f'PART B: Variance of Fine Aggregate Component = {pvariance(fine_ag_data)} kg/m^3 mixture')
+    print(f'PART B: Standard Deviation of Fine Aggregate Component = {pstdev(fine_ag_data)} kg/m^3 mixture\n')
 
     # Find the mean and standard error of the cement component (PART C)
     print(f'PART C: Mean of Cement Component = {mean(cement_data)} kg/m^3 mixture')
-    print(f'PART C: Standard Error of Cement Component = {stdev(cement_data) / math.sqrt(len(cement_data))} kg/m^3 mixture\n')
+    print(f'PART C: Standard Error of Cement Component = {pstdev(cement_data) / math.sqrt(len(cement_data))} kg/m^3 mixture\n')
 
     # Find the mean and standard error of the concrete compressive strength (PART D)
     print(f'PART D: Mean of Concrete Compressive Strength = {mean(strength_data)} MPa')
-    print(f'PART D: Standard Error of Concrete Compressive Strength = {stdev(strength_data) / math.sqrt(len(strength_data))} MPa')
+    print(f'PART D: Standard Error of Concrete Compressive Strength = {pstdev(strength_data) / math.sqrt(len(strength_data))} MPa')
 
 # entry point
 if __name__ == "__main__":
